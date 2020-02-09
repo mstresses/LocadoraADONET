6cdb4c5c85867b6e5bf17316db5483ef3d7e1255
anne nicolle zimmermann <annezdz@hotmail.com>
2020-02-09 17:17:03 -0200

    First Commit - Included Mappings

---
+++ DataAccessLayer/Mappings/ClienteMapConfig.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Mappings
+{
+    internal class ClienteMapConfig : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteMapConfig()
+        {
+            this.ToTable("CLIENTES");
+            this.Property(c => c.Nome).HasMaxLength(70);
+            this.Property(c => c.CPF).IsFixedLength().HasMaxLength(14);
+            this.Property(c => c.DataNascimento).HasColumnType("date").IsRequired();
+
+        }
+    }
+}
+
---
+++ DataAccessLayer/Mappings/LocacaoMapConfig.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Mappings
+{
+    internal class LocacaoMapConfig : EntityTypeConfiguration<Locacao>
+    {
+        public LocacaoMapConfig()
+        {
+            this.ToTable("LOCACOES");
+            this.Property(l => l.DataDevolucao).HasColumnType("date").IsRequired();
+            this.Property(l => l.DataLocacao).HasColumnType("date").IsRequired();
+            this.Property(l => l.DataPrevistaDevolucao).HasColumnType("date").IsRequired();
+
+        }
+    }
+}
