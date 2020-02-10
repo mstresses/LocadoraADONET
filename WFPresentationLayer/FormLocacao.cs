9b1cfaaf9bfdba606aa646c01e74922d4ae61b91
anne nicolle zimmermann <annezdz@hotmail.com>
2020-02-10 15:48:00 -0300

    Commit Sala Anne 1

--- WFPresentationLayer/FormLocacao.cs
+++ WFPresentationLayer/FormLocacao.cs
@@ -21,6 +21,21 @@ namespace WFPresentationLayer
         public FormLocacao()
         {
             InitializeComponent();
+            dataGridView1.CellDoubleClick += DataGridView1_CellDoubleClick;
+            DataResponse<Cliente> response = svc.GetData();
+            if (response.Sucesso)
+            {
+                dataGridView1.DataSource = response.Data;
+            }
+            else
+            {
+                MessageBox.Show(response.GetErrorMessage());
+            }
+        }
+        ClienteService svc = new ClienteService();
+        private void DataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void btnPesquisaCLiente_Click(object sender, EventArgs e)
